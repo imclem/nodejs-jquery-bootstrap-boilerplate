495a89b09d0734c28cb1a15bcf065174cb99ed99
Clement Agarini <716892+imclem@users.noreply.github.com>
2018-10-28 14:33:13 +0100

    Fix port logging

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import app from "./app";
 var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
-  console.log("Server started on port 3000!");
+  console.log("Server started on port" + port);
 });
